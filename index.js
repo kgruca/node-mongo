0ad903936271b999c65663d356e62cab435ae4aa
Krzysztof Gruca <kgruca21@gmail.com>
2022-12-26 23:27:43 -0800

    operations: insertDocument, findDocuments, removeDocument, updateDocument

--- index.js
+++ index.js
@@ -5,5 +5,35 @@ const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+
     assert.strictEqual(err, null);
+
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbname);
+
+    db.dropCollection('campsites', (err, result) => {
+        assert.strictEqual(err, null);
+        console.log('Dropped Collection', result);
+
+        const collection = db.collection('campsites');
+
+        collection.insertOne(
+            {
+                name: "Breadcrumb Trail Campground", 
+                description: "Test"
+            },
+            (err, result) => {
+                assert.strictEqual(err, null);
+                console.log('Insert Document:', result.ops);
+
+                collection.find().toArray((err, docs) => {
+                    assert.strictEqual(err, null);
+                    console.log('Found documents:', docs);
+
+                    client.close();
+                });
+            }
+        );
+    });
 });
